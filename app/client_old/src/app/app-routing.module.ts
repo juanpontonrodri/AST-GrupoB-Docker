ad11c15fa937115d9d999d37f1ade6d5235a3f2a
juan <juanpontonrodri@gmail.com>
2023-03-07 19:59:21 +0100

    rehaciendo

--- app/client_old/src/app/app-routing.module.ts
+++ app/client_old/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonViewComponent } from './pages/pokemon-view/pokemon-view.component';
+const routes: Routes = [
+  { path: '', component: PokemonViewComponent },
+  { path: 'pokemon', component: PokemonViewComponent },
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/client_old/src/app/app.module.ts
+++ app/client_old/src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonViewComponent } from './pages/pokemon-view/pokemon-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonViewComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
